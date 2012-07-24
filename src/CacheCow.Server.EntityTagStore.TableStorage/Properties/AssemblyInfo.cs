ab9f98fc2dca012480e2bf1b7fb188d8a9951b81
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-07-24 22:10:12 +0300

    initialized Server.EntityTagStore.TableStorage proj, installed necessary nuget packages for that project and taken project dependency on CacheCow.Common

---
+++ src/CacheCow.Server.EntityTagStore.TableStorage/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("CacheCow.Server.EntityTagStore.TableStorage")]
+[assembly: AssemblyDescription("Windows Azure Table Storage integration library for CacheCow EntityTagStore")]
