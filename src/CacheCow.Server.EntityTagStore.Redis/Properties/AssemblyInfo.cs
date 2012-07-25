a519b6dd2befd76757fe0b3ac73114cb2437756e
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-07-25 03:43:27 +0300

    initialized Server.EntityTagStore.Redis proj, installed necessary nuget packages for that project and taken project dependency on CacheCow.Common

---
+++ src/CacheCow.Server.EntityTagStore.Redis/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("CacheCow.Server.EntityTagStore.SqlServer")]
+[assembly: AssemblyDescription("Redis integration library for CacheCow EntityTagStore")]
